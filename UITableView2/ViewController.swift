6d5afae70be4f2d14b38d3185441a36cebf0e954
Miranda Melina <mmiranda@yopapp.me>
2019-12-16 16:08:37 -0300

    add tableview2

---
+++ UITableView2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UITableView2
+//
+//  Created by Melina Miranda on 16/12/2019.
+//  Copyright © 2019 Melina Miranda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableViewCell: UITableViewCell!
+    @IBOutlet weak var CountryName: UILabel!
+    @IBOutlet weak var CityName: UILabel!
+    @IBOutlet weak var MapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    
+    }
+
+
+}
+
