a2fb3ae6a685832bcf9cf14c12111d1dafbe9357
Miranda Melina <mmiranda@yopapp.me>
2019-12-17 17:51:58 -0300

    add mapview

--- UITableView2/ViewController.swift
+++ UITableView2/ViewController.swift
@@ -8,47 +8,62 @@
 
 import UIKit
 import MapKit
-class ViewController: UIViewController, MKMapViewDelegate{
+class ViewController: UIViewController, MKMapViewDelegate, UITableViewDelegate, UITableViewDataSource {
+   
     
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var tableViewCell: UITableViewCell!
-    @IBOutlet weak var CountryName: UILabel!
-    @IBOutlet weak var CityName: UILabel!
-    @IBOutlet weak var MapView: MKMapView!
+    @IBOutlet weak var countryName: UILabel!
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
-    struct name {
-        var CountryName: String
-        var CityName: String
-        var lattitude: CLLocationDegrees
+    
+    
+    struct City {
+        var countryName: String
+        var cityName: String
+        var latitude: CLLocationDegrees
         var longitude: CLLocationDegrees
     }
     
-    var Names = [name]()
+    var cities = [City]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        Names = [
+        cities = [
             
-            name.init(CountryName: "Brazil", CityName: "Brasilia", lattitude: -15.7797200, longitude: 4.895168),
+            City.init( countryName: "Brazil", cityName: "Brasilia", latitude: -15.7797200, longitude: -47.9297200),
             
-            name.init(CountryName: "UK", CityName: "London", lattitude: 51.5085300, longitude: -0.1257400 ),
-            
-            name.init(CountryName: "Nueva Zelanda", CityName: "Auckland", lattitude: -36.8500000, longitude: 174.7833300 ),
-            
-            name.init(CountryName: "Australia", CityName: "Canberra", lattitude: -35.2834587, longitude: 149.128067 ),
-            
-            name.init(CountryName: "Netherlands", CityName: "Amsterdam", lattitude: 52.370216, longitude: 4.895168),
+            City.init(countryName: "UK", cityName: "London", latitude: 51.5085300, longitude: -0.1257400),
         
+            City.init(countryName: "Nueva Zelanda", cityName: "Auckland", latitude: -36.8500000, longitude: 174.7833300 ),
+            
+            City.init(countryName: "Australia", cityName: "Canberra", latitude: -35.2834587, longitude: 149.128067 ),
+            
+            City.init(countryName: "Netherlands", cityName: "Amsterdam", latitude: 52.370216, longitude: 4.895168),
         ]
+
+       
+      
     }
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        var names = mapView.dequeueReusableAnnotationView(withIdentifier: "IDCell", for: )
-   
-    return names
-    }
+    
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return cities.count
+     }
      
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "IDcell", for: indexPath)
+        cell = UITableViewCell(style: .value1, reuseIdentifier: "IDcell")
+
+        
+        cell.textLabel?.text = cities[indexPath.row].cityName
+        cell.detailTextLabel?.text = cities[indexPath.row].countryName
+        
+     return cell
     }
     
-
-    
     
 
 
+}
