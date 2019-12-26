64591899699f13ffefd8273e12cef1e171f5c54b
Miranda Melina <mmiranda@yopapp.me>
2019-12-26 15:40:00 -0300

    update project user data

---
+++ UITableView2/CityTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  CityTableViewCell.swift
+//  UITableView2
+//
+//  Created by Melina Miranda on 18/12/2019.
+//  Copyright © 2019 Melina Miranda. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class CityTableViewCell: UITableViewCell, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var countryName: UILabel!
+
+    
+   
+    func showCity(city: City){
+        let center = CLLocationCoordinate2DMake(city.latitude, city.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+           let region = MKCoordinateRegion(center: center, span: span)
+           mapView.setRegion(region, animated: true)
+       
+        let annotations = MKPointAnnotation()
+        annotations.coordinate = CLLocationCoordinate2DMake(city.latitude, city.longitude)
+        annotations.title = city.cityName
+        annotations.subtitle = city.countryName
+          
+        mapView.addAnnotation(annotations)
+    }
+
+}
--- UITableView2/ViewController.swift
+++ UITableView2/ViewController.swift
@@ -8,60 +8,58 @@
 
 import UIKit
 import MapKit
-class ViewController: UIViewController, MKMapViewDelegate, UITableViewDataSource, UITableViewDelegate {
-    
-    @IBOutlet weak var tableViewCell: UITableViewCell!
-    @IBOutlet weak var CountryName: UILabel!
-    @IBOutlet weak var CityName: UILabel!
-    @IBOutlet weak var MapView: MKMapView!
-    @IBOutlet weak var tableView: UITableView!
-    
-        
-    struct City {
-        var countryName: String
-        var cityName: String
-        var latitude: CLLocationDegrees
-        var longitude: CLLocationDegrees
-    }
 
+
+
+struct City {
+       var countryName: String
+       var cityName: String
+       var latitude: CLLocationDegrees
+       var longitude: CLLocationDegrees
+  }
+
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+   
+    @IBOutlet weak var tableView: UITableView!
     
     var cities = [City]()
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
         cities = [
-            
-            City.init(countryName: "Brazil", cityName: "Brasilia", latitude: -15.7797200, longitude: 4.895168),
-            
+        
+            City.init (countryName: "Brazil", cityName: "Brasilia", latitude: -15.7797200, longitude:-47.9297200),
+        
             City.init(countryName: "UK", cityName: "London", latitude: 51.5085300, longitude: -0.1257400 ),
-            
-            City.init(countryName: "Nueva Zelanda", cityName: "Auckland", latitude: -36.8500000, longitude: 174.7833300 ),
-            
-            City.init(countryName: "Australia", cityName: "Canberra",latitude: -35.2834587, longitude: 149.128067 ),
-            
+        
+            City.init(countryName: "New Zealand", cityName: "Auckland", latitude: -36.8500000, longitude: 174.7833300 ),
+        
+            City.init(countryName: "Australia", cityName: "Canberra", latitude: -35.2834587, longitude: 149.128067 ),
+        
             City.init(countryName: "Netherlands", cityName: "Amsterdam", latitude: 52.370216, longitude: 4.895168),
-        
-        ]
-    }
-      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-              return cities.count
-       }
-       
-      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-          var cell = tableView.dequeueReusableCell(withIdentifier: "IDcell", for: indexPath)
-        
-            CLLocationCoordinate2DMake(cities[indexPath.row].latitude, cities[indexPath.row].longitude)
-
-        cell = UITableViewCell(style: .subtitle, reuseIdentifier: "IDcell")
-          cell.textLabel?.text = cities[indexPath.row].cityName
-          cell.detailTextLabel?.text = cities[indexPath.row].countryName
-          
-       return cell
-      }
-
+                   
+    ]
+     
+        tableView.rowHeight = 80
+      
     }
     
-
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return cities.count
+           }
     
-    
+           
+           func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CityTableViewCell", for: indexPath) as! CityTableViewCell
+            
+            cell.showCity(city: City(countryName: cities[indexPath.row].countryName, cityName: cities[indexPath.row].cityName, latitude: cities[indexPath.row].latitude, longitude: cities[indexPath.row].longitude))
+            
+            cell.cityName.text = cities[indexPath.row].cityName
+            cell.countryName.text = cities[indexPath.row].countryName
+            
+            return cell
+           }
+        
+    }
